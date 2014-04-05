e93a816433082f2a51da94d362a87354d0664bac
DiabloHorn <diablohorn@gmail.com>
2014-04-05 23:19:06 +0200

    initial checkin of a project to do simple HTTP with C & WinInet

---
+++ simple_http/simple_http.h
@@ -0,0 +1,6 @@
+/*
+DiabloHorn http://diablohorn.wordpress.com
+*/
+#include <Windows.h>
+DWORD http_get_content(char *target, unsigned char **response_data);
+DWORD http_post_binary(char *target, unsigned char *post_data, int post_data_length, unsigned char **response_data);
---
+++ simple_http/test_simple_http.c
@@ -0,0 +1,30 @@
+/*
+DiabloHorn http://diablohorn.wordpress.com
+*/
+#include "simple_http.h"
+#include <stdio.h>
+/*
+TODO:
+support ssl
+*/
+int main(int argc, char *argv[]){
+	unsigned char *http_response_content = NULL;
+	HANDLE tempfile = NULL;
+	DWORD tempfilesize = 0;
+	unsigned char *readbuffer;
+	DWORD readbytes = 0;
+	DWORD http_response_code = -1;
+
+	tempfile = CreateFile("../a.dll", GENERIC_READ, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+	tempfilesize = GetFileSize(tempfile, NULL);
+	readbuffer = malloc(tempfilesize);
+	ReadFile(tempfile, readbuffer, tempfilesize, &readbytes, NULL);
+	CloseHandle(tempfile);
+	http_response_code = http_post_binary("http://10.50.0.122:8008/", readbuffer, tempfilesize, &http_response_content);
+	printf("%d\n", http_response_code);
+	printf("%s\n", http_response_content);
+	http_response_code = http_get_content("http://10.50.0.122:8008/", &http_response_content);
+	printf("%d\n", http_response_code);
+	printf("%s\n", http_response_content);
+
+}
